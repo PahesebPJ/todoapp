e3d625a94564c045af9844efb6c3d2214c2ff1a9
paheseb <pahesebw@gmail.com>
2022-03-08 21:49:57 -0600

    Projects component bar updated, remove icon project component and add a scrollbar

--- src/components/projectsComponent/ProjectsComponent.js
+++ src/components/projectsComponent/ProjectsComponent.js
@@ -4,16 +4,18 @@ import axios from 'axios';
 //Styles
 import './ProjectsComponent.css'
 
+//Icons
+
 function ProjectsComponent() {
 
-    const [toggle, setToggle] = React.useState(false);
+    /* const [toggle, setToggle] = React.useState(false); */
     const [projects, setProjects] = React.useState([]);
 
-    const handleClick = () => {
+    /* const handleClick = () => {
         let icon = document.querySelector('ion-icon');
         toggle ? icon.style.transform = 'rotate(90deg)' : icon.style.transform = 'rotate(180deg)';
         setToggle(!toggle);
-    }
+    } */
 
     useEffect(() => {
         axios.get('http://192.168.0.2:4000/api/userprojects/1').then(response => {
@@ -28,19 +30,21 @@ function ProjectsComponent() {
 
     return (
         <div className='container'>
-            <ion-icon name="chevron-forward-outline" onClick={handleClick}></ion-icon>
+            {/* <ion-icon name="chevron-forward-outline" onClick={handleClick}></ion-icon> */}
             {
                 projects.map(project => (
                     <div className="project__card__container" key={project.id}>
                         <img
                             className="project__card__img"
-                            alt="Hola mami que quieres quMe la voy a jalar equis de kqajaj"
+                            alt=""
                             src={project.picture}
-                            style={toggle ? projectCardContaineratDisappear : {}}
+                        /*  style={toggle ? projectCardContaineratDisappear : {}} */
                         />
                     </div>
+
                 ))
             }
+
         </div>
     )
 }
